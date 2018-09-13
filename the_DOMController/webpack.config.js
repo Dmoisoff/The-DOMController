b667c1155e2ee66622fa9705e1576a037faa206e
Daniel Moisoff <dmoisoff@gmail.com>
2018-09-12 20:32:36 -0400

    renamed files

--- the_DOMController/webpack.config.js
+++ the_DOMController/webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
   entry: "./lib/main.js",
   output: {
     path: __dirname,
-    filename: "./lib/jquery_lite.js"
+    filename: "./lib/DOMController.js"
 	},
 	devtool: "source-map"
 };
